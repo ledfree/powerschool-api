e1eec49c7785e61ec80cbf89bd81587710d8852e
Fred Lee <ledfree@gmail.com>
2024-01-27 23:29:56 -0800

    get token working

--- psapi.go
+++ psapi.go
@@ -1,11 +1,21 @@
 package psapi
 
 import (
+	"bytes"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
+	"time"
 )
+
+type tokenResponse struct {
+	Access_Token string `json:"access_token"`
+	Token_Type   string `json:"token_type"`
+	Expires_In   string `json:"expires_in"`
+}
 
 type ApiConfig struct {
 	PsUrl        string
@@ -13,28 +23,56 @@ type ApiConfig struct {
 	ClientSecret string
 }
 
-func validateConfig() error {
-
-	return nil
+func (a ApiConfig) toBase64() string {
+	return base64.StdEncoding.EncodeToString([]byte(fmt.Sprintf("%s:%s", a.ClientId, a.ClientSecret)))
 }
 
-func GetAccessToken(psurl, clientId, clientSecret string) (status int, token string, err error) {
+func (a ApiConfig) GetAccessToken() (status int, token string, err error) {
 	status = 400 // bad request
 
-	str64 := base64.StdEncoding.EncodeToString([]byte(fmt.Sprintf("%s:%s", clientId, clientSecret)))
-	u, err := url.Parse(psurl)
+	u, err := url.Parse(a.PsUrl)
+	if err != nil {
+		err = fmt.Errorf("invalid URL %s : %s", a.PsUrl, err)
+		return
+	}
+
+	urlToken := fmt.Sprintf("https://%s/oauth/access_token/", u.Hostname())
+	body := []byte(`{"body": "grant_type=client_credentials"}`)
+	req, err := http.NewRequest(http.MethodPost, urlToken, bytes.NewBuffer(body))
+	if err != nil {
+		return
+	}
+	req.Header.Add("Authorization", fmt.Sprintf("Basic %s", a.toBase64()))
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
+
+	client := http.Client{
+		Timeout: 30 * time.Second,
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return
+	}
+	defer res.Body.Close()
+
+	status = res.StatusCode
+
+	if res.StatusCode != http.StatusOK {
+		return
+	}
+
+	resBody, err := io.ReadAll(res.Body)
 	if err != nil {
 		return
 	}
 
-	resp, err := http.Post(u.Hostname(), "applicaion/json", nil)
+	var t tokenResponse
+	err = json.Unmarshal(resBody, &t)
 	if err != nil {
 		return
 	}
-	status = resp.StatusCode
-	token = resp.Body
 
-	token = u.Hostname()
+	token = t.Access_Token
 
 	return
 }
--- psapi_test.go
+++ psapi_test.go
@@ -6,4 +6,12 @@ import (
 
 func TestGetToken(t *testing.T) {
 	ps := ApiConfig{TestUrl, TestClientId, TestClientSecret}
+
+	s, token, err := ps.GetAccessToken()
+	if err != nil {
+		t.Fatalf("fail : GetAccessToken - %s", err)
+	} else {
+		t.Logf("Status       : %d", s)
+		t.Logf("Bearer Token : %s", token)
+	}
 }

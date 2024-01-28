4901c821dbcfe18fa9225cb796d39209777faba6
Fred Lee <ledfree@gmail.com>
2024-01-27 20:48:14 -0800

    init testing

--- psapi.go
+++ psapi.go
@@ -1,14 +1,38 @@
 package psapi
 
-import "net/url"
+import (
+	"encoding/base64"
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+type ApiConfig struct {
+	PsUrl        string
+	ClientId     string
+	ClientSecret string
+}
+
+func validateConfig() error {
+
+	return nil
+}
 
 func GetAccessToken(psurl, clientId, clientSecret string) (status int, token string, err error) {
 	status = 400 // bad request
 
+	str64 := base64.StdEncoding.EncodeToString([]byte(fmt.Sprintf("%s:%s", clientId, clientSecret)))
 	u, err := url.Parse(psurl)
 	if err != nil {
 		return
 	}
+
+	resp, err := http.Post(u.Hostname(), "applicaion/json", nil)
+	if err != nil {
+		return
+	}
+	status = resp.StatusCode
+	token = resp.Body
 
 	token = u.Hostname()
 
---
+++ psapi_test.go
@@ -0,0 +1,9 @@
+package psapi
+
+import (
+	"testing"
+)
+
+func TestGetToken(t *testing.T) {
+	ps := ApiConfig{TestUrl, TestClientId, TestClientSecret}
+}
